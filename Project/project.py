ca7ce3769f24de6c3d0997da2f80b8a92188ca34
mac40 <marcobgg4@gmail.com>
2020-01-27 19:20:45 +0100

    V2.0 first completed version progress bar added

--- Project/project.py
+++ Project/project.py
@@ -9,11 +9,12 @@ mat: 1179124
 '''
 import argparse
 
-import editdistance
 import numpy as np
 from nltk import jaccard_distance as jd
+from nltk import edit_distance as ed
 from pm4py.algo.filtering.log.variants import variants_filter
 from pm4py.objects.log.importer.xes import factory as xes_import_factory
+from progress.bar import Bar
 
 parser = argparse.ArgumentParser(
     description='Compute variability of the event log')
@@ -41,23 +42,26 @@ def compute_edit_distance_variability(log, n=100):
         n = number of traces to analyze
     output:
         average edit distance between traces of the log
-
-    comment:
-        for testing purposes, the number of traces to be analyzed
-        has been reduced to 100 in order to reduce the computational
-        timers and get a faster result
     '''
-    distances = []
-    for index, trace_one in enumerate(log[0:n]):
-        for trace_two in log[index+1:n]:
-            distance = editdistance.eval(str(trace_one.attributes),
-                                         str(trace_two.attributes))
-        distances.append(distance)
+    with Bar('Edit distance', max=n*(n-1)/2, suffix='%(percent)d%%') as bar:
+        distances = []
+        for index, trace_one in enumerate(log[0:n]):
+            events_one = []
+            for event in trace_one:
+                events_one.append(event["concept:name"])
+            events_one = ' '.join(events_one)
+            for trace_two in log[index+1:n]:
+                events_two = []
+                for event in trace_two:
+                    events_two.append(event["concept:name"])
+                events_two = ' '.join(events_two)
+                distances.append(ed(events_one, events_two))
+                bar.next()
 
     return np.average(distances)
 
 
-def compute_my_variability(log, n=100):
+def compute_my_variability(log, n=100, suffix='%(percent)d%%'):
     '''
     compute a measure of variability defined by me
     ---Jaccard distance---
@@ -67,12 +71,18 @@ def compute_my_variability(log, n=100):
     output:
         average Jaccard distance between traces of the log
     '''
-    distances = []
-    for index, trace_one in enumerate(log[0:n]):
-        for trace_two in log[index+1:n]:
-            distance = jd(set(str(trace_one.attributes)),
-                          set(str(trace_two.attributes)))
-        distances.append(distance)
+    with Bar('Jaccard distance', max=n*(n-1)/2) as bar:
+        distances = []
+        for index, trace_one in enumerate(log[0:n]):
+            events_one = []
+            for event in trace_one:
+                events_one.append(event["concept:name"])
+            for trace_two in log[index+1:n]:
+                events_two = []
+                for event in trace_two:
+                    events_two.append(event["concept:name"])
+                distances.append(jd(set(events_one), set(events_two)))
+                bar.next()
 
     return np.average(distances)
 
@@ -86,10 +96,19 @@ if __name__ == "__main__":
     print("Number of variants in the event log: {}"
           .format(compute_variant_variability(log)))
 
-    n = input("Select the number of traces to analyze (default 100): ")
+    n = input("Select the number of traces to analyze (default 5): ")
 
-    if not n:
-        n = 100
+    '''
+    for testing purposes, the number of traces to be analyzed
+    has been reduced by default to 5 in order to reduce the computational
+    time and get a faster result
+    '''
+    if not n or n == "1":
+        if n == "1":
+            print("minimum traces required = 2")
+        print("number of traces set to 5 by default")
+        n = 5
+    n = int(n)
 
     print("Average edit distance between {} traces of the event log: {}"
           .format(n, compute_edit_distance_variability(log, n)))
